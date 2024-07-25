a26a5d994403c2227088363dbb0f0b6255b11ca4
Aryndraa <seruputbusiness@gmail.com>
2024-07-25 11:25:14 +0800

    upload project

---
+++ index.php
@@ -0,0 +1,62 @@
+<?php 
+
+$luasPersegi = 3;
+
+// persegi
+for ($coloum = 0; $coloum < $luasPersegi; $coloum++) {
+    for ($row = 0; $row < $luasPersegi; $row++) {
+        echo "◯";
+    }
+    echo "<br>";
+}
+
+echo "<br/>";
+
+//donut
+for ($coloum = 0; $coloum < $luasPersegi; $coloum++) {
+    for ($row = 0; $row < $luasPersegi; $row++) {
+        if ($coloum == 1 && $row == 1) {
+            echo " # ";
+        } else {
+            echo "◯";
+        }
+    }
+    echo "<br>";
+}
+
+$luasSegitiga = 5;
+
+//segitiga siku"
+for ($coloum = 0; $coloum <= $luasSegitiga; $coloum++) {
+    for ($row = 0; $row < $coloum; $row++) {
+        echo "◯ ";
+    }
+    echo "<br>";
+}
+
+echo "<br/>";
+
+//segitiga siku terbalik
+for ($coloum = $luasSegitiga  ; $coloum >= 1; $coloum--) {
+    for ($row = 0; $row < $coloum; $row++) {
+        echo "◯ ";
+    }
+    echo "<br>";
+}
+
+for ($coloum = 0; $coloum <= $luasSegitiga; $coloum++) {
+    for ($row = 0; $row < $coloum; $row++) {
+        if($coloum == 3 && $row == 1) {
+            echo "✕. ";
+        } elseif(($coloum == 4) && ($row == 2) || 
+                 ($coloum == 4) && ($row == 1)) {
+            echo "✕. ";
+        }else {
+            echo "◯ ";
+        }
+    }
+    echo "<br>";
+}
+
+
+
