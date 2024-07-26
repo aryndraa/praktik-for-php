950c403297ea0ad8dc02c3a4288b62893defa2ec
Aryndraa <seruputbusiness@gmail.com>
2024-07-26 11:51:29 +0800

    revision

--- index.php
+++ index.php
@@ -1,10 +1,10 @@
 <?php 
 
-$luasPersegi = 3;
+$squareArea = 3;
 
-// persegi
-for ($coloum = 0; $coloum < $luasPersegi; $coloum++) {
-    for ($row = 0; $row < $luasPersegi; $row++) {
+// square
+for ($coloum = 0; $coloum < $squareArea; $coloum++) {
+    for ($row = 0; $row < $squareArea; $row++) {
         echo "◯";
     }
     echo "<br>";
@@ -13,50 +13,54 @@ for ($coloum = 0; $coloum < $luasPersegi; $coloum++) {
 echo "<br/>";
 
 //donut
-for ($coloum = 0; $coloum < $luasPersegi; $coloum++) {
-    for ($row = 0; $row < $luasPersegi; $row++) {
+for ($coloum = 0; $coloum < $squareArea; $coloum++) {
+    for ($row = 0; $row < $squareArea; $row++) {
+        
         if ($coloum == 1 && $row == 1) {
             echo " # ";
         } else {
             echo "◯";
         }
+
     }
     echo "<br>";
 }
 
-$luasSegitiga = 5;
+$triangleArea = 5;
 
 //segitiga siku"
-for ($coloum = 0; $coloum <= $luasSegitiga; $coloum++) {
+for ($coloum = 0; $coloum <= $triangleArea; $coloum++) {
     for ($row = 0; $row < $coloum; $row++) {
         echo "◯ ";
     }
+
     echo "<br>";
 }
 
 echo "<br/>";
 
 //segitiga siku terbalik
-for ($coloum = $luasSegitiga  ; $coloum >= 1; $coloum--) {
+for ($coloum = $triangleArea  ; $coloum >= 1; $coloum--) {
     for ($row = 0; $row < $coloum; $row++) {
         echo "◯ ";
     }
+
     echo "<br>";
 }
 
-for ($coloum = 0; $coloum <= $luasSegitiga; $coloum++) {
+for ($coloum = 0; $coloum <= $triangleArea; $coloum++) {
+
     for ($row = 0; $row < $coloum; $row++) {
+
         if($coloum == 3 && $row == 1) {
             echo "✕. ";
         } elseif(($coloum == 4) && ($row == 2) || 
                  ($coloum == 4) && ($row == 1)) {
             echo "✕. ";
-        }else {
+        } else {
             echo "◯ ";
         }
+
     }
     echo "<br>";
 }
-
-
-
